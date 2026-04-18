b553d3b8d983045cd28612320e346c46605de30d
YulianaChambi <chambiy447@gmail.com>
2026-04-18 00:11:54 -0500

    feat: Implement Student Progress logic ✨

--- learncode-catalog/src/main/java/com/learncode_backend/repository/CourseModuleRepository.java
+++ learncode-catalog/src/main/java/com/learncode_backend/repository/CourseModuleRepository.java
@@ -1,10 +1,18 @@
 package com.learncode_backend.repository;
 
 import com.learncode_backend.model.CourseModule;
+
+import feign.Param;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 import java.util.UUID;
 
 public interface CourseModuleRepository extends JpaRepository<CourseModule, UUID> {
     List<CourseModule> findByCourseIdOrderByModuleOrderAsc(UUID courseId);
+    
+    @Query("SELECT COUNT(m) FROM CourseModule m WHERE m.courseId = :courseId")
+    long countByCourseId(@Param("courseId") UUID courseId);
 }
